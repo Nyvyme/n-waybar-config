708c83d5273617fe63b1ba98be2fa16a22a738f9
Nyvyme <mixagame228@gmail.com>
2024-03-21 13:32:19 +0000

    Initial commit

---
+++ launch.sh
@@ -0,0 +1,14 @@
+if [ -f $HOME/.cache/waybar-disabled ] ;then 
+    killall waybar
+    pkill waybar
+    exit 1 
+fi
+
+# ----------------------------------------------------- 
+# Quit all running waybar instances
+# ----------------------------------------------------- 
+killall waybar
+pkill waybar
+sleep 0.2
+
+waybar
